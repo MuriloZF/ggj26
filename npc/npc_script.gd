17c605c5f2563f161b4904757ac67b8bb0f4d427
Hyam-M <ranteater2005@gmail.com>
2026-01-31 01:00:42 -0300

    Created npc spawn system

---
+++ npc/npc_script.gd
@@ -0,0 +1,56 @@
+extends Area2D
+var Enemy = preload("res://npc/npc.tscn")
+
+var npcMax = 15
+var enemies = []
+
+enum npcState {
+		idle,
+		speaking	
+	}
+
+var stats = npcState.idle
+
+func npc_spawn():
+	for i in range(npcMax):
+		var enemy = Enemy.instantiate()
+		
+		var valid_position = false
+		var x : int
+		var y : int
+		
+		while not valid_position:
+			valid_position = true
+			x = randi() % 640
+			y = randi() % 410
+			
+			for e in enemies:
+				if x == int(e.position.x) and y == int(e.position.y):
+					valid_position = false
+					break
+		
+		enemy.position = Vector2(x, y)
+		add_child(enemy)
+		enemies.append(enemy)
+
+func idle_state():
+	pass
+	
+func speaking_state():
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	npc_spawn()
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	match stats:
+		npcState.idle: 
+			idle_state()
+		npcState.speaking:
+			speaking_state()
+		
+		
+	
