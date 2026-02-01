7acead308baf16581d0106c25c071d07dba14cf8
MuriloZF <mzimermanfortaleza@gmail.com>
2026-02-01 05:34:28 -0300

    TODO: Create a prettier menu

--- menu.gd
+++ menu.gd
@@ -6,7 +6,7 @@ func _process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	$mouse.position = mouse_pos	
 	if dentro_da_area and Input.is_action_just_pressed("select"):
-		get_tree().change_scene_to_file("res://boss_fight.tscn")
+		get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_jogar_area_entered(area: Area2D) -> void:
 	dentro_da_area = true
